297abac3e64864533d8314708607cd5d37548b81
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2022-03-22 22:08:48 +0100

    fix overlay style

--- src/components/BookingProcessingOverlay.tsx
+++ src/components/BookingProcessingOverlay.tsx
@@ -1,7 +1,7 @@
 export const BookingProcessingOverlay: React.FC = () => {
   return (
-    <div className="fixed inset-0 z-10 bg-black bg-opacity-50 w-screen h-screen flex flex-col">
-      <div className="flex flex-col text-center justify-center font-bold text-white text-3xl">
+    <div className="fixed inset-0 z-10 bg-black bg-opacity-50 w-screen h-screen flex flex-col justify-center">
+      <div className="flex flex-col text-center font-bold text-white text-3xl">
         <span>Placing</span> <span>Booking</span> <span>...</span>
       </div>
     </div>
